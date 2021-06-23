fa4ce3571f55e4c6b9701cf3ca2d5792dca1cf91
felixk <felix.kosasih@lazada.com>
2021-06-24 03:09:58 +0800

    Changes for heroku

--- src/app.js
+++ src/app.js
@@ -9,10 +9,17 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
+const buildPath = path.join(__dirname, "client/build");
+app.use(express.static(buildPath));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(buildPath));
+}
+
 // Define Routes
 app.use('/api/url', require('./routes/router'));
 app.use('/', require('./routes/router'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server is running on port ${PORT}`));
